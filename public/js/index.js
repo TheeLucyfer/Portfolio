c0a3606e5f3692f4b7d52203824a5fe77457fc29
TheeLucy <62519677+TheeLucyfer@users.noreply.github.com>
2020-09-09 21:12:30 -0700

    Add header background scrolling, and fixed position. Also add old profile that I'm going to burn

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,20 @@
-console.log("hi")
+let header;
+window.addEventListener("load", (ev)=>{
+    header = document.querySelector(".header")
+})
+
+
+
+window.addEventListener("scroll", (ev) =>{
+    if (!header)
+        return;
+    let viewHeight = window.innerHeight;
+    let scrollY = window.scrollY;
+    let headerHeight = header.offsetHeight;
+
+    if(viewHeight -headerHeight <= scrollY){  
+        header.classList.add("scrolled")
+    }else{
+        header.classList.remove("scrolled")
+    }
+})
